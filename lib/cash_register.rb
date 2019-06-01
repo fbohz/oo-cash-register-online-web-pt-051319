ddbfc375ba855c576bdfde01d5f9a99e54b4d3c3
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-01 05:48:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,6 +27,6 @@ class CashRegister
     @discount? "After the discount, the total comes to $#{self.total.to_i}." : "There is no discount to apply."
   end 
   
-  
+  def void_last_transaction
   
 end   
